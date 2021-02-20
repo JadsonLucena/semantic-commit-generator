1dd33a2a7769cf8ce3fed0a1fe85b76a296a19c6
JadsonLucena <jadsonlucena@gmail.com>
2021-02-20 14:18:11 -0300

    feat (type): insertion of extra field in others
    Only if it is necessary to create a new optional type

--- assets/script/index.js
+++ assets/script/index.js
@@ -5,3 +5,27 @@ var body = document.querySelector('body > main > section#body');
 var footer = document.querySelector('body > main > section#footer');
 var significantChange = document.querySelector('body > main > section#significantChange');
 var result = document.querySelector('body > main > section#result');
+
+
+for (let checkBox of type.querySelectorAll('form > div > label > input')) {
+
+    checkBox.onchange = function() {
+
+        if (this.value == 'other') {
+
+            if (!type.querySelector('form > div > label[for="other"] > span > input')) {
+
+                type.querySelector('form > div > label[for="other"] > span').innerHTML = '<input type="text" name="otherType" pattern="[a-zA-Z]+" placeholder="Insert the new type" required>';
+                type.querySelector('form > div > label[for="other"] > span > input').focus();
+
+            }
+
+        } else {
+
+            type.querySelector('form > div > label[for="other"] > span').innerHTML = "<strong>other</strong>: Doesn't fit any of the suggested types?";
+
+        }
+
+    };
+
+}
