6c959ce4b09562c39ba96771328234d7205c883a
JadsonLucena <jadsonlucena@gmail.com>
2021-02-20 14:16:33 -0300

    feat: get the sessions

---
+++ assets/script/index.js
@@ -0,0 +1,7 @@
+var type = document.querySelector('body > main > section#type');
+var scope = document.querySelector('body > main > section#scope');
+var summary = document.querySelector('body > main > section#summary');
+var body = document.querySelector('body > main > section#body');
+var footer = document.querySelector('body > main > section#footer');
+var significantChange = document.querySelector('body > main > section#significantChange');
+var result = document.querySelector('body > main > section#result');
